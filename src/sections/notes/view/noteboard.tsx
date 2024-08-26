87949933662f1446d3cd084833a416254629c195
huyton <huyton110798@gmail.com>
2024-08-26 23:09:31 +0200

    feat: :sparkles: note board

--- src/sections/notes/view/noteboard.tsx
+++ src/sections/notes/view/noteboard.tsx
@@ -1,14 +1,22 @@
 import { useNotes } from "@/context/NotesProvider";
-import { NoteBoardProps } from "@/types/section";
-import { List } from "@mantine/core";
+import { truncateText } from "@/utils";
+import { Accordion, Box, List, ScrollArea } from "@mantine/core";
 
 export default function NoteBoard() {
   const { notes } = useNotes();
 
   const notesList = notes.map((note) => (
-    <List key={"note"}>
-      <List.Item>{note.text}</List.Item>
-    </List>
+    <Accordion.Item key={note.id} value={truncateText(note.text, 50)}>
+      <Accordion.Control>{truncateText(note.text, 20)}</Accordion.Control>
+      <Accordion.Panel>{note.text}</Accordion.Panel>
+    </Accordion.Item>
   ));
-  return <>{notesList}</>;
+
+  return (
+    <Box>
+      <ScrollArea type="never" offsetScrollbars={false}>
+        <Accordion>{notesList}</Accordion>
+      </ScrollArea>
+    </Box>
+  );
 }
