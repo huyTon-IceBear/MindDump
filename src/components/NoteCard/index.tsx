8c28d80fd83dbe8a2d8aa11a29cd5f90fe8f9c0f
huyton <huyton110798@gmail.com>
2025-03-18 12:16:00 +0100

    fix: :bug: display note card when no media files found

--- src/components/NoteCard/index.tsx
+++ src/components/NoteCard/index.tsx
@@ -12,7 +12,6 @@ import { useState } from "react";
 
 export default function NoteCard({ note, handleClick }: NoteCardProps) {
   const [hovered, setHovered] = useState(false);
-
   const totalMediaFiles = note.mediaFiles.length;
 
   return (
@@ -25,23 +24,24 @@ export default function NoteCard({ note, handleClick }: NoteCardProps) {
       onMouseLeave={() => setHovered(false)}
       onClick={handleClick}
     >
-      <Card.Section>
-        <AspectRatio ratio={16 / 9} maw={400} mx="auto" pos="relative">
-          <Image
-            src={note.mediaFiles[0].src}
-            alt="Media file"
-            style={{ width: "100%", height: "100%", objectFit: "cover" }}
-          />
-          {totalMediaFiles > 1 && (
-            <Overlay color="rgba(0,0,0,.4)" backgroundOpacity={0.65}>
-              <Center style={{ height: "100%" }}>
-                <Title>{"+" + `${totalMediaFiles - 1}`}</Title>
-              </Center>
-            </Overlay>
-          )}
-        </AspectRatio>
-      </Card.Section>
-
+      {totalMediaFiles > 0 && (
+        <Card.Section>
+          <AspectRatio ratio={16 / 9} maw={400} mx="auto" pos="relative">
+            <Image
+              src={note.mediaFiles[0].src}
+              alt="Media file"
+              style={{ width: "100%", height: "100%", objectFit: "cover" }}
+            />
+            {totalMediaFiles > 1 && (
+              <Overlay color="rgba(0,0,0,.4)" backgroundOpacity={0.65}>
+                <Center style={{ height: "100%" }}>
+                  <Title>{"+" + `${totalMediaFiles - 1}`}</Title>
+                </Center>
+              </Overlay>
+            )}
+          </AspectRatio>
+        </Card.Section>
+      )}
       <Text size="sm" c="dimmed" mt="md" mb="xs">
         {note.text}
       </Text>
